59d2d42ca6439390fe77200b0954d862678337d7
Emil Folino <emilfolino@gmail.com>
2018-12-10 11:23:14 +0100

    Added me and nav components

---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+<nav class="links">
+    <a href="">Me</a>
+    <a href="">Redovisning</a>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Me',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
